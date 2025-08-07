d1e6e53b164240be2fcad02c227b26e766dbd125
AMALRAJ N J <amalrajnj007@gmail.com>
2025-08-07 22:34:16 +0200

    initial setup done

---
+++ Reactjs/movie-app/src/Components/Search.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Search({ searchTerm, setSearchTerm }) {
+  return (
+    <div className="search">
+      <div>
+        <img src="search.svg" alt="" />
+        <input
+          type="text"
+          placeholder="Search your favourite movies..."
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Search;
